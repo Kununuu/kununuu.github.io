24d66db8683eecc304242f231d30750bfc20bf10
Thomas <thomas@hyhyve.com>
2025-05-25 22:49:12 +0300

    SEO enhancements mit sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kununuu.github.io",
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
 });
